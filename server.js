dfada75cba1a9ca16885a3aeb9ef83cb0dee4693
3alkou3 <159251144+3alkou3@users.noreply.github.com>
2024-07-04 21:08:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const routes = require("./routes");
 const path = require("path")
 const app = express();
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://panoramacafe.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cors({
   origin: "*"
 }));
