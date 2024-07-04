f4d294eeb7e66dfc8ab4fce66d17c87d776d2957
3alkou3 <159251144+3alkou3@users.noreply.github.com>
2024-07-04 23:35:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://panoramacafe.netlify.app/"); // Update to match the requesting origin
+    res.header("Access-Control-Allow-Origin", "https://panoramacafe.netlify.app"); // Update to match the requesting origin
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
